1fb3d75f1c3f98e8feb7c893c19178c15213fded
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-12-16 17:57:28 +0100

    chore(config): ajustes en la configuración de la aplicación
    - Actualización de parámetros generales para optimizar el rendimiento.
    - Simplificación de variables de entorno y rutas.
    - Revisión y validación de la estructura del archivo de configuración.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    BASE_API_URL: process.env.BASE_API_URL || "https://fallback-url.com/api",
+  },
 };
 
 export default nextConfig;
