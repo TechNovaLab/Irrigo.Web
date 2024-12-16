088f8420796513b349939f700d70ae0b681c7981
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2024-12-16 20:17:06 +0100

    feat(domain): ✨ Actualización de modelos y repositorios en CropType
    - src/domain/models/CropType.ts: Se elimina propiedad innecesaria.
    - src/domain/repositories/cropTypeRepository.ts: Se ajusta la función getCropTypes() para reflejar los cambios en el modelo.

--- src/domain/repositories/cropTypeRepository.ts
+++ src/domain/repositories/cropTypeRepository.ts
@@ -8,6 +8,10 @@ class CropTypeRepository extends BaseRepository {
   async createCropType(request: CreateCropTypeRequest): Promise<CropType> {
     return this.create("crops/create-crop-type", request);
   }
+
+  async getCropTypes(): Promise<CropType[]> {
+    return this.getAll("crops/");
+  }
 }
 
 export const cropTypeRepository = new CropTypeRepository(createApiClient(() => identityManager.getUserIdentity()));
