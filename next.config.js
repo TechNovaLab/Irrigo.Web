df3b2226c3446e4837facd8415564fa16b099644
Luis Rodriguez Dominguez <luis.rodriguez.dominguez@outlook.com>
2025-04-26 08:43:12 +0200

    chore(deps): 🔄 actualizar dependencias a versiones más recientes
    - Next.js a ^15.3.1, React y React-DOM a ^19.1.0, react-icons a ^5.5.0 y uuid a ^9.0.1.
    - DevDependencies: @types/node ^20.17.31, @types/react ^18.3.20, @types/react-dom ^18.3.6, @types/uuid ^10.0.0, eslint ^8.57.1, eslint-config-next ^14.1.0, postcss ^8.5.3, tailwindcss ^3.4.17 y typescript ^5.8.3.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   env: {
     BASE_API_URL: process.env.BASE_API_URL || "https://fallback-url.com/api",
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
